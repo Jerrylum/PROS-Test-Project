01a55a0450f16cd24bc7db0eec657d44bc957f90
Jerry Lum <serverofjerry@gmail.com>
2025-01-17 02:29:54 +0800

    Add fail check and test escape chars

--- src/main.test.cpp
+++ src/main.test.cpp
@@ -12,6 +12,7 @@ void vexSystemExitRequest(void);
 
 void test_initialize() {
     pros::c::serctl(SERCTL_DISABLE_COBS, nullptr);
+    printf("Running ' \" tests\n"); // test escape
     utest_main(0, nullptr);
     pros::delay(100); // wait for the console to print
     vexSystemExitRequest();
