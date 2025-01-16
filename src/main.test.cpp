0e0c18e2215d286600ea49c19856d25ddebd0dbc
Jerry Lum <serverofjerry@gmail.com>
2025-01-17 02:16:52 +0800

    Add test cases

---
+++ src/main.test.cpp
@@ -0,0 +1,18 @@
+// // #include "main.h"
+
+#include "pros/apix.h"
+#include "utest.h"
+
+UTEST_STATE();
+
+extern "C" {
+
+void vexSystemExitRequest(void);
+}
+
+void test_initialize() {
+    pros::c::serctl(SERCTL_DISABLE_COBS, nullptr);
+    utest_main(0, nullptr);
+    pros::delay(100); // wait for the console to print
+    vexSystemExitRequest();
+}
---
+++ src/my-library.test.cpp
@@ -0,0 +1,22 @@
+#include "pros/apix.h"
+#include "utest.h"
+
+UTEST(foo, bar) {
+    std::cout << pros::millis() << std::endl;
+    std::cout << vexSystemTimeGet() << std::endl;
+    pros::delay(1000);
+    std::cout << pros::millis() << std::endl;
+    std::cout << vexSystemTimeGet() << std::endl;
+
+    ASSERT_TRUE(1);
+}
+
+UTEST(foo, bar2) {
+    std::cout << pros::millis() << std::endl;
+    std::cout << vexSystemTimeGet() << std::endl;
+    pros::delay(1000);
+    std::cout << pros::millis() << std::endl;
+    std::cout << vexSystemTimeGet() << std::endl;
+
+    ASSERT_TRUE(0);
+}
